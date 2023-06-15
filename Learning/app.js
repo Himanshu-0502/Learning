00044b53afc4a8ff0b4bb29cf3979738e3e159cc
Himanshu-0502 <cfhimanshu0502@gmail.com>
2023-06-15 17:13:05 +0530

    modified:   Learning/app.js

--- Learning/app.js
+++ Learning/app.js
@@ -11,3 +11,23 @@ let sex=null;
 console.log(sex);
 let a=['0','1','2'];
 console.log(a);
+let num=[1,3,6,3,5,7];
+console.log(num[0]+num[1]);
+console.log(num.sort());
+num.push(5);
+console.log(num);
+let mix=['1',1,true];
+console.log(mix);
+let obj={
+    n:'Himanshu Sharma',
+    s:'Male',
+    i:0
+}
+console.log(obj.n);
+let emily={
+    age1:16,
+    school:'DPS',
+    class:'10th std',
+    subjects:['Maths','Physics','English','Social Studies']
+}
+console.log(emily.subjects[2]);
